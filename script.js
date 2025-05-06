0436fe9007f8fd85c8fcd60d01bcc2c326162619
Aniyah123101 <aniyahhall1231@gmail.com>
2025-05-05 21:54:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Portfolio JS loaded!");
+
+document.querySelector('#contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you for reaching out!');
+});
